fda0d9cd0d2dc1e8c0ec426c65af969fd2b4436c
Erik Moberg <extern.tretton37-moberg@universal-investment.com>
2022-04-15 23:18:23 +0200

    startup

--- pres/startup.js
+++ pres/startup.js
@@ -7,21 +7,17 @@ import { HttpAdapter } from './repositories/http-adapter.js';
 import { Config } from './cross-cutting/config.js';
 import { SlideView } from './components/slide-view.js';
 
-(() => {
+// register services
+ServiceLocator.register(SlideService.name, () => new SlideService(ServiceLocator.resolve(SlideRepository.name)));
+ServiceLocator.register(SlideRepository.name, () => new SlideRepository(ServiceLocator.resolve(HttpAdapter.name), ServiceLocator.resolve(Config.name)));
+ServiceLocator.register(HttpAdapter.name, () => new HttpAdapter());
+ServiceLocator.register(Config.name, () => new Config());
 
-        // register services
-        ServiceLocator.register(SlideService.name, () => new SlideService(ServiceLocator.resolve(SlideRepository.name)));
-        ServiceLocator.register(SlideRepository.name, () => new SlideRepository(ServiceLocator.resolve(HttpAdapter.name), ServiceLocator.resolve(Config.name)));
-        ServiceLocator.register(HttpAdapter.name, () => new HttpAdapter());
-        ServiceLocator.register(Config.name, () => new Config());
+// register components
+customElements.define('slide-presenter', SlidePresenter);
+customElements.define('slide-view', SlideView);
+customElements.define('gimmick-thingy', GimmickThingy);
 
-        // register components
-        customElements.define('slide-presenter', SlidePresenter);
-        customElements.define('slide-view', SlideView);
-        customElements.define('gimmick-thingy', GimmickThingy);
-
-        // add components to page
-        document.body.appendChild(document.createElement('slide-presenter'));
-        document.body.appendChild(document.createElement('gimmick-thingy'));
-    }
-)();
+// add components to page
+document.body.appendChild(document.createElement('slide-presenter'));
+document.body.appendChild(document.createElement('gimmick-thingy'));
